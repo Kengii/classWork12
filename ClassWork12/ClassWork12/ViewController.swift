43a1be74e4ad23eb3313d8f0cd8bef52be7b0798
Martynov Evgeny <martynov@wimix.by>
2022-01-25 19:33:48 +0300

    Project created

---
+++ ClassWork12/ClassWork12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassWork12
+//
+//  Created by Martynov Evgeny on 25.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
